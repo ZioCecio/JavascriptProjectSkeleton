55288a3212f9ccdb1b9f7681387b8082969d1bb1
Gabriele Ceresara <gabricetro@libero.it>
2019-01-25 23:01:09 +0100

    prealfacheforsefunziona

--- pokedex/src/Components/Spinner.js
+++ pokedex/src/Components/Spinner.js
@@ -10,13 +10,13 @@ export default class Spinner extends Component {
         return(
             <div className="pure-g spinner-container">
                 <div className="pure-u-1-5 center-element">
-                    <p>{this.props.name}</p>
+                    <p className="spinner-name">{this.props.name}</p>
                 </div>
                 <div className="pure-u-3-5">
                     <input className="range select-form" id={this.props.name} type="range" min="0" max="252" step="4" onChange={() => this.props.usingSpinner(this.props.name)} defaultValue={this.props.evs} />
                 </div>
                 <div className="pure-u-1-5 center-element">
-                    <p>{this.props.evs}</p>
+                    <p className="spinner-name">{this.props.evs}</p>
                 </div>
             </div>
         )
